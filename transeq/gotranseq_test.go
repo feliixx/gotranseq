c3a740c1549159be5a04283f20a45993bf02ea21
adrienpetel <peteladrien@gmail.com>
2018-12-15 11:59:04 +1100

    test refactoring
      - remove dependency to testtify/assert
      - use subtest

--- transeq/gotranseq_test.go
+++ transeq/gotranseq_test.go
@@ -3,14 +3,12 @@ package transeq_test
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"strings"
 	"testing"
 
 	"github.com/feliixx/gotranseq/transeq"
 	"github.com/jessevdk/go-flags"
-	"github.com/stretchr/testify/assert"
 )
 
 type testParameters struct {
@@ -18,48 +16,66 @@ type testParameters struct {
 	Expected string `json:"expected"`
 }
 
-func getOptionsAndName(opts string) (string, transeq.Options, error) {
-	list := strings.Split(opts, " ")
-	for i := range list {
-		list[i] = strings.Join([]string{string("-"), list[i]}, "")
-	}
-	var options transeq.Options
-	_, err := flags.ParseArgs(&options, list)
-	if err != nil {
-		return "", options, err
-	}
-
-	return strings.Replace(strings.Join(list, "_"), "--", "", -1), options, nil
-}
-
 func TestAllOptions(t *testing.T) {
-	// read the config file to run test
+
 	data, err := ioutil.ReadFile("testdata/data.json")
-	assert.Nil(t, err)
+	if err != nil {
+		t.Error(err)
+	}
+
 	var param []testParameters
 	err = json.Unmarshal(data, &param)
-	assert.Nil(t, err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	resultBuffer := bytes.NewBuffer(make([]byte, 0))
 	iohandler := transeq.IOHandler{
 		Out: resultBuffer,
 	}
-	// fasta sequences to translate
+
 	fasta, err := ioutil.ReadFile("testdata/test.fna")
-	assert.Nil(t, err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	for _, p := range param {
 
-		fastaReader := bytes.NewReader(fasta)
-		iohandler.In = fastaReader
+		t.Run(p.Options, func(t *testing.T) {
 
-		name, opts, err := getOptionsAndName(p.Options)
-		opts.NumWorker = 1
-		assert.Nil(t, err)
-		fmt.Printf("running test %s\n", name)
-		err = iohandler.ReadSequenceAndTranslate(opts)
-		assert.Nil(t, err)
-		assert.Equal(t, p.Expected, string(resultBuffer.Bytes()), name)
-		resultBuffer.Reset()
+			fastaReader := bytes.NewReader(fasta)
+			iohandler.In = fastaReader
+
+			opts, err := getOptionsAndName(p.Options)
+			opts.NumWorker = 1
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = iohandler.ReadSequenceAndTranslate(opts)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if want, got := p.Expected, resultBuffer.String(); want != got {
+				t.Errorf("expected %s\nbut got\n%s\n", want, got)
+			}
+
+			resultBuffer.Reset()
+		})
 	}
+}
+
+func getOptionsAndName(opts string) (options transeq.Options, err error) {
+
+	// convert emboss transeq flag (single '-' prefix) to gotranseq flags
+	flagOpts := strings.Split(opts, " ")
+	for i, flag := range flagOpts {
+		if strings.HasPrefix(flag, "-") {
+			flagOpts[i] = strings.Replace(flag, "-", "--", 1)
+		}
+	}
+	_, err = flags.ParseArgs(&options, flagOpts)
+
+	return options, err
 }
