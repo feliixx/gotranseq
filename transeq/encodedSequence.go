146cb3a562d087b2799a2035be155fb5abed9c3e
feliixx <peteladrien@gmail.com>
2021-03-16 11:21:03 +1100

    fix crash when sequence contains ambiguous nucleotide
    if ambiguous nucleotide, replace it with 'N'
    
    updates #3

--- transeq/encodedSequence.go
+++ transeq/encodedSequence.go
@@ -32,10 +32,9 @@ func newEncodedSequence(buf *bytes.Buffer, headerSize int) encodedSequence {
 			s[headerSize+i] = gCode
 		case 'T', 'U':
 			s[headerSize+i] = tCode
-		case 'N':
-			s[headerSize+i] = nCode
 		default:
-			fmt.Printf("WARNING: invalid char in sequence %s: %s, ignoring", s[headerSize:], string(s[headerSize+i]))
+			s[headerSize+i] = nCode
+			fmt.Printf("WARNING: invalid char in sequence %s: '%s' ( pos %d), replacing with 'N'\n", string(s[:headerSize]), string(n), i)
 		}
 	}
 	return s
